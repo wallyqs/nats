16febaa5686f65bab9e9ec60defbd7fb34f51f8f
Waldemar Quevedo <wally@synadia.com>
2021-04-08 14:46:18 -0700

    Data race with Msg based APIs
    Signed-off-by: Waldemar Quevedo <wally@synadia.com>

--- test/headers_test.go
+++ test/headers_test.go
@@ -14,6 +14,9 @@
 package test
 
 import (
+	"fmt"
+	"net/http"
+	"net/http/httptest"
 	"reflect"
 	"testing"
 	"time"
@@ -120,5 +123,110 @@ func TestNoHeaderSupport(t *testing.T) {
 
 	if _, err := nc.RequestMsg(m, time.Second); err != nats.ErrHeadersNotSupported {
 		t.Fatalf("Expected an error, got %v", err)
+	}
+}
+
+func TestMsgHeadersAcrossHandlers(t *testing.T) {
+	s := RunServerOnPort(-1)
+	defer s.Shutdown()
+
+	nc, err := nats.Connect(s.ClientURL())
+	if err != nil {
+		t.Fatalf("Error connecting to server: %v", err)
+	}
+	defer nc.Close()
+
+	// Validate that headers are preserved across hops.
+	errCh := make(chan error, 2)
+	msgCh := make(chan *nats.Msg, 1)
+	sub, err := nc.Subscribe("nats.svc.A", func(m *nats.Msg) {
+		// A -> B
+		m.Subject = "nats.svc.B"
+		m.Header.Add("X-Result-A", "A")
+		resp, err := nc.RequestMsg(m, 2*time.Second)
+		if err != nil {
+			errCh <- err
+			return
+		}
+
+		// Respond to request from HTTP Handler.
+		resp.Subject = m.Reply
+		err = nc.PublishMsg(resp)
+		if err != nil {
+			errCh <- err
+			return
+		}
+	})
+	defer sub.Unsubscribe()
+
+	sub, err = nc.Subscribe("nats.svc.B", func(m *nats.Msg) {
+		m.Header.Add("X-Result-B", "B")
+		m.Subject = m.Reply
+
+		// B -> A
+		err := nc.PublishMsg(m)
+		if err != nil {
+			errCh <- err
+			return
+		}
+	})
+	defer sub.Unsubscribe()
+
+	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		msg := nats.NewMsg("nats.svc.A")
+		msg.Header = r.Header.Clone()
+		resp, err := nc.RequestMsg(msg, 2*time.Second)
+		if err != nil {
+			errCh <- err
+			return
+		}
+		msgCh <- resp
+
+		// Add headers to HTTP handler.
+		for k, v := range resp.Header {
+			w.Header()[k] = v
+		}
+
+		// Remove Date for testing.
+		w.Header()["Date"] = nil
+
+		w.WriteHeader(200)
+		fmt.Fprintln(w, string(resp.Data))
+	}))
+	defer ts.Close()
+
+	req, err := http.NewRequest("GET", ts.URL, nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+	req.Header.Add("X-Begin-Request", "start")
+
+	client := &http.Client{Timeout: 3 * time.Second}
+	resp, err := client.Do(req)
+	if err != nil {
+		t.Fatal(err)
+	}
+	var msg *nats.Msg
+	select {
+	case <-time.After(1 * time.Second):
+		t.Fatal("Timeout waiting for message.")
+	case err = <-errCh:
+		if err != nil {
+			t.Fatal(err)
+		}
+	case msg = <-msgCh:
+	}
+
+	result := resp.Header.Get("X-Result-A")
+	if result != "A" {
+		t.Errorf("Unexpected header value, got: %+v", result)
+	}
+	result = resp.Header.Get("X-Result-B")
+	if result != "B" {
+		t.Errorf("Unexpected header value, got: %+v", result)
+	}
+
+	if len(msg.Header) != 5 {
+		t.Errorf("Wrong number of headers in NATS message, got: %v", len(msg.Header))
 	}
 }
